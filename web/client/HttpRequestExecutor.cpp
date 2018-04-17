dd9795daf927cf7337c346d1af562ec65deafa3e
lganzzzo <lganzzzo@gmail.com>
2018-04-18 01:33:19 +0300

    HttpRequestExecutor::executeAsync

--- web/client/HttpRequestExecutor.cpp
+++ web/client/HttpRequestExecutor.cpp
@@ -112,31 +112,87 @@ oatpp::async::Action HttpRequestExecutor::executeAsync(oatpp::async::AbstractCor
   
   class ExecutorCoroutine : public oatpp::async::CoroutineWithResult<ExecutorCoroutine, std::shared_ptr<HttpRequestExecutor::Response>> {
   private:
+    std::shared_ptr<oatpp::network::ClientConnectionProvider> m_connectionProvider;
     String::PtrWrapper m_method;
     String::PtrWrapper m_path;
     std::shared_ptr<Headers> m_headers;
     std::shared_ptr<Body> m_body;
   private:
     std::shared_ptr<oatpp::data::stream::IOStream> m_connection;
+    std::shared_ptr<oatpp::data::buffer::IOBuffer> m_ioBuffer;
+    void* m_bufferPointer;
+    os::io::Library::v_size m_bufferBytesLeftToRead;
   public:
     
-    ExecutorCoroutine(const String::PtrWrapper& method,
+    ExecutorCoroutine(const std::shared_ptr<oatpp::network::ClientConnectionProvider>& connectionProvider,
+                      const String::PtrWrapper& method,
                       const String::PtrWrapper& path,
                       const std::shared_ptr<Headers>& headers,
                       const std::shared_ptr<Body>& body)
-      : m_method(method)
+      : m_connectionProvider(connectionProvider)
+      , m_method(method)
       , m_path(path)
       , m_headers(headers)
       , m_body(body)
     {}
     
     Action act() override {
-      return _return(nullptr);
+      oatpp::network::ClientConnectionProvider::AsyncCallback callback =
+      static_cast<oatpp::network::ClientConnectionProvider::AsyncCallback>(&ExecutorCoroutine::onConnectionReady);
+      return m_connectionProvider->getConnectionAsync(this, callback);
+    }
+    
+    Action onConnectionReady(const std::shared_ptr<oatpp::data::stream::IOStream>& connection) {
+      m_connection = connection;
+      auto request = oatpp::web::protocol::http::outgoing::Request::createShared(m_method, m_path, m_headers, m_body);
+      request->headers->put(oatpp::web::protocol::http::Header::HOST, m_connectionProvider->getHost());
+      m_ioBuffer = oatpp::data::buffer::IOBuffer::createShared();
+      auto upStream = oatpp::data::stream::OutputStreamBufferedProxy::createShared(connection, m_ioBuffer);
+      m_bufferPointer = m_ioBuffer->getData();
+      m_bufferBytesLeftToRead = m_ioBuffer->getSize();
+      return request->sendAsync(this, upStream->flushAsync(this, yieldTo(&ExecutorCoroutine::readResponse)), upStream);
+    }
+    
+    Action readResponse() {
+      return oatpp::data::stream::IOStream::
+      readSomeDataAsyncInline(m_connection.get(), m_bufferPointer, m_bufferBytesLeftToRead, yieldTo(&ExecutorCoroutine::parseResponse));
+    }
+    
+    Action parseResponse() {
+      
+      os::io::Library::v_size readCount = m_ioBuffer->getSize() - m_bufferBytesLeftToRead;
+      
+      if(readCount > 0) {
+        
+        oatpp::parser::ParsingCaret caret((p_char8) m_ioBuffer->getData(), m_ioBuffer->getSize());
+        auto line = protocol::http::Protocol::parseResponseStartingLine(caret);
+        if(!line){
+          return error("Invalid starting line");
+        }
+        
+        oatpp::web::protocol::http::Status err;
+        auto headers = protocol::http::Protocol::parseHeaders(caret, err);
+        
+        if(err.code != 0){
+          return error("can't parse headers");
+        }
+        
+        auto bodyStream = oatpp::data::stream::InputStreamBufferedProxy::createShared(m_connection,
+                                                                                      m_ioBuffer,
+                                                                                      caret.getPosition(),
+                                                                                      (v_int32) readCount);
+        
+        return _return(Response::createShared(line->statusCode, line->description, headers, bodyStream));
+        
+      }
+      
+      return error("Read zero bytes from Response");
+      
     }
     
   };
   
-  return parentCoroutine->startCoroutineForResult<ExecutorCoroutine>(callback, method, path, headers, body);
+  return parentCoroutine->startCoroutineForResult<ExecutorCoroutine>(callback, m_connectionProvider, method, path, headers, body);
   
 }
   
