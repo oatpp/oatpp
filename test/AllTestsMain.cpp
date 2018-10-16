a167fc6748469e260b3e0aea9aaf29b66c444162
lganzzzo <lganzzzo@gmail.com>
2018-10-16 19:13:54 +0300

    build script v1

--- test/AllTestsMain.cpp
+++ test/AllTestsMain.cpp
@@ -6,9 +6,30 @@
 #include "oatpp/test/parser/json/mapping/DTOMapperPerfTest.hpp"
 #include "oatpp/test/parser/json/mapping/DTOMapperTest.hpp"
 
-#ifdef OATPP_ENABLE_ALL_TESTS_MAIN
-int main() {
-  oatpp::base::Environment::init();
+#include "oatpp/core/concurrency/SpinLock.hpp"
+#include "oatpp/core/base/Environment.hpp"
+
+#include <iostream>
+
+namespace {
+
+class Logger : public oatpp::base::Logger {
+private:
+  oatpp::concurrency::SpinLock::Atom m_atom;
+public:
+  
+  Logger()
+  : m_atom(false)
+  {}
+  
+  void log(v_int32 priority, const std::string& tag, const std::string& message) override {
+    oatpp::concurrency::SpinLock lock(m_atom);
+    std::cout << tag << ":" << message << "\n";
+  }
+  
+};
+
+void runTests() {
   OATPP_RUN_TEST(oatpp::test::collection::LinkedListTest);
   OATPP_RUN_TEST(oatpp::test::memory::MemoryPoolTest);
   OATPP_RUN_TEST(oatpp::test::memory::PerfTest);
@@ -16,7 +37,31 @@ int main() {
   OATPP_RUN_TEST(oatpp::test::parser::json::mapping::DeserializerTest);
   OATPP_RUN_TEST(oatpp::test::parser::json::mapping::DTOMapperPerfTest);
   OATPP_RUN_TEST(oatpp::test::parser::json::mapping::DTOMapperTest);
+}
+  
+}
+
+#ifdef OATPP_ENABLE_ALL_TESTS_MAIN
+int main() {
+  
+  oatpp::base::Environment::init();
+  oatpp::base::Environment::setLogger(new Logger());
+  
+  runTests();
+  
+  oatpp::base::Environment::setLogger(nullptr);
   oatpp::base::Environment::destroy();
+  
+  /* Print how much objects were created during app running, and what have left-probably leaked */
+  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+  std::cout << "\nEnvironment:\n";
+  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
+  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
+  
+  OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
+  
+  oatpp::base::Environment::destroy();
+  
   return 0;
 }
 #endif
