cf5140cdd4ecb5474d028e42c80df6eb51301f8c
lganzzzo <lganzzzo@gmail.com>
2019-08-02 03:08:43 +0400

    less tests

--- test/oatpp/AllTestsMain.cpp
+++ test/oatpp/AllTestsMain.cpp
@@ -79,8 +79,8 @@ void runTests() {
 */
   {
 
-    oatpp::test::web::FullTest test_virtual(0, 1000);
-    test_virtual.run();
+//    oatpp::test::web::FullTest test_virtual(0, 1000);
+//    test_virtual.run();
 
 //    oatpp::test::web::FullTest test_port(8000, 10);
 //    test_port.run();
@@ -100,7 +100,7 @@ void runTests() {
   {
 
     oatpp::test::web::FullAsyncClientTest test_virtual(0, 1000);
-    test_virtual.run(20);
+    test_virtual.run(3);
 
 //    oatpp::test::web::FullAsyncClientTest test_port(8000, 10);
 //    test_port.run(1);
