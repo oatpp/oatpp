9df8c6f870be7582b8cf3e468292ee0b60d9c635
lganzzzo <lganzzzo@gmail.com>
2020-08-12 02:59:23 +0300

    orm::QueryResult. Better interface.

--- src/oatpp/orm/QueryResult.hpp
+++ src/oatpp/orm/QueryResult.hpp
@@ -34,13 +34,25 @@ public:
 
   virtual ~QueryResult() = default;
 
-  virtual bool isSuccess() = 0;
+  virtual bool isSuccess() const = 0;
 
-  virtual v_int64 count() = 0;
+  virtual v_int64 position() const = 0;
 
-  virtual std::vector<std::vector<oatpp::Void>> fetchRows(v_int64 count) = 0;
+  virtual v_int64 count() const = 0;
 
   virtual void fetch(oatpp::Void& polymorph, v_int64 count) = 0;
+
+  template<class Wrapper>
+  Wrapper fetch(v_int64 count) {
+    oatpp::Void polymorph(Wrapper::Class::getType());
+    fetch(polymorph, count);
+    return polymorph.template staticCast<Wrapper>();
+  }
+
+  template<class Wrapper>
+  Wrapper fetch() {
+    return fetch<Wrapper>(this->count());
+  }
 
 };
 
