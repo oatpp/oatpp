e8bc2b07591c3991afdb26ad74ec25206cdb8181
lganzzzo <lganzzzo@gmail.com>
2020-10-08 00:26:03 +0300

    Tests: uncomment all tests.

--- test/oatpp/AllTestsMain.cpp
+++ test/oatpp/AllTestsMain.cpp
@@ -74,7 +74,7 @@ void runTests() {
   OATPP_LOGD("Tests", "coroutine size=%d", sizeof(oatpp::async::AbstractCoroutine));
   OATPP_LOGD("Tests", "action size=%d", sizeof(oatpp::async::Action));
   OATPP_LOGD("Tests", "class count=%d", oatpp::data::mapping::type::ClassId::getClassCount());
-/*
+
   OATPP_RUN_TEST(oatpp::test::base::CommandLineArgumentsTest);
   OATPP_RUN_TEST(oatpp::test::base::LoggerTest);
 
@@ -104,10 +104,10 @@ void runTests() {
   OATPP_RUN_TEST(oatpp::test::core::data::mapping::type::AnyTest);
   OATPP_RUN_TEST(oatpp::test::core::data::mapping::type::EnumTest);
   OATPP_RUN_TEST(oatpp::test::core::data::mapping::type::ObjectTest);
-*/
+
   OATPP_RUN_TEST(oatpp::test::core::data::mapping::type::InterpretationTest);
   OATPP_RUN_TEST(oatpp::test::core::data::mapping::TypeResolverTest);
-/*
+
   OATPP_RUN_TEST(oatpp::test::async::LockTest);
   OATPP_RUN_TEST(oatpp::test::parser::CaretTest);
 
@@ -198,7 +198,7 @@ void runTests() {
     test_port.run();
 
   }
-*/
+
 }
 
 }
